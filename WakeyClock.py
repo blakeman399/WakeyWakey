752645d8c4fd441b58c28e4224510b306a53101e
Blake Mastrud <blakejosephmastrud@gmail.com>
2016-05-06 10:53:05 -0600

    Update WakeyClock.py
    Added the ability to select the time of alarm and what URL to play at startup

--- WakeyClock.py
+++ WakeyClock.py
@@ -1,15 +1,20 @@
+import os
 import time
 import datetime
 import subprocess
 
+alarmTime = str(raw_input("What time would you like me to wake you? "))
+
+youtubeURL = str(raw_input("YouTube URL? "))
+
+
 
 while True:
     currentTime = time.strftime("%H:%M")
-    if(currentTime == '09:17'):
-        print("hello")
-        #Play Youtube URL in audio mode with VLC
-        subprocess.Popen("VLC --no-video https://youtu.be/lsJLLEwUYZM?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG", shell=False)
+    if(currentTime == alarmTime ):
+        subprocess.Popen("C:\\Users\\bmastrud\\Downloads\\mps\\VLC.exe --no-video " + youtubeURL, shell=False)
         time.sleep(60)
     else:
         print(currentTime)
         time.sleep(1)
+        print(alarmTime)
