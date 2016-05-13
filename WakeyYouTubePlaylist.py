76d7edc125c83b250499c03863d22d824433647c
Blake Mastrud <blakejosephmastrud@gmail.com>
2016-05-13 15:10:46 -0600

    Update WakeyYouTubePlaylist.py

--- WakeyYouTubePlaylist.py
+++ WakeyYouTubePlaylist.py
@@ -1,20 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
-import os
-import sys
-import time
-import contextlib
+from WakeyCore import youtubePlayList, playerVLC
 
-def youtubePlayList(url):
-    #Removes old playlist file-Supresses error if file does not exist
-    with contextlib.suppress(FileNotFoundError):
-        os.remove("c:\\logs\\goats.txt") 
-    URL = url
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "html.parser")
-    links = soup.find_all("tr", {"class": "pl-video"})
-    time.sleep(2)
-    for link in links:
-        #print(link.get("data-title"))
-        log = open("c:\\logs\\goats.txt", "a")
-        print("https://www.youtube.com/watch?v=" + link.get("data-video-id"), file = log)
+youList = input("Youtube URL? ")
+playList = "C:\\logs\\goats.txt"
+youtubePlayList(youList)
+playerVLC(playList)
