3bee72415ed1e6032845d9e405d07bebbb3183be
Blake Mastrud <blakejosephmastrud@gmail.com>
2016-05-19 14:33:28 -0600

    Cleanup formatting

--- WakeyCore.py
+++ WakeyCore.py
@@ -6,25 +6,30 @@ import re
 import os
 import contextlib
 
-#Windows Path
+# Windows Path
 filePath = "C:\\logs\\"
 vlcPlayer = "C:\\Users\\bmastrud\Desktop\\vlc-2.2.3\\"
 
-#NPR Hourly Podcast Parser
+
+# NPR Hourly Podcast Parser
 def PodcastParser(url):
-    print("Starting Requests"); url = urllib.request.urlopen(url)
-    print("Request Done"); content = url.read()
-    print("Start Parsing"); soup = BeautifulSoup(content, "html.parser")
-    #Find first mp3 link(newest)
+    print("Starting Requests");
+    url = urllib.request.urlopen(url)
+    print("Request Done");
+    content = url.read()
+    print("Start Parsing");
+    soup = BeautifulSoup(content, "html.parser")
+    # Find first mp3 link(newest)
     variable = soup.find('a', href=re.compile('http.*\.mp3'))['href']
     print("Done Parsing", variable)
     playerVLC(variable)
 
-#YouTubeParser - Creates txt file with playlists urls
+
+# YouTubeParser - Creates txt file with playlists urls
 def youtubePlayList(url):
-    #Removes old playlist file-Supresses error if file does not exist
+    # Removes old playlist file-Supresses error if file does not exist
     with contextlib.suppress(FileNotFoundError):
-        os.remove(filePath +"playList.txt")
+        os.remove(filePath + "playList.txt")
 
     url = urllib.request.urlopen(url)
     content = url.read()
@@ -32,11 +37,12 @@ def youtubePlayList(url):
     links = soup.find_all("tr", {"class": "pl-video"})
     time.sleep(2)
     for link in links:
-        #print(link.get("data-title"))
-        log = open(filePath +"playList.txt", "a")
-        print("https://www.youtube.com/watch?v=" + link.get("data-video-id"), file = log)
+        # print(link.get("data-title"))
+        log = open(filePath + "playList.txt", "a")
+        print("https://www.youtube.com/watch?v=" + link.get("data-video-id"), file=log)
 
-#RSSParser - Finds Headlines
+
+# RSSParser - Finds Headlines
 def RssFeed(RssURL):
     with contextlib.suppress(FileNotFoundError):
         os.remove(filePath + "rss.txt")
@@ -47,13 +53,16 @@ def RssFeed(RssURL):
     for item in items:
         title = item.find('title').text
         log = open(filePath + "rss.txt", "a")
-        print(title, file = log)
-#Google TTS
+        print(title, file=log)
+
+
+# Google TTS
 def GTTS(txt):
     tts = gTTS(text=txt, lang='en')
     tts.save(filePath + "hello.mp3")
 
-#VLCFunction
+
+# VLCFunction
 def playerVLC(Media):
     print("Starting VLC")
     os.system(vlcPlayer + "vlc " + Media)
